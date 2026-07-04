6ef90ca7a50cb5d2f6aea61946d32e30c57ec52a
Angelica Soto <a1,angiesoto@gmail.com>
2026-07-04 23:40:26 +0200

    Add semantic structure for components & SEO meta tags

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from '../assets/images/logo.jpg'
+
+function Header() {
+return(
+    <header>
+        <img src={logo} alt="Little Lemon logo" />
+    </header>
+);
+
+}
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+    return(
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservations">Reservations</a></li>
+                <li><a href="/orden-online">Order online</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
