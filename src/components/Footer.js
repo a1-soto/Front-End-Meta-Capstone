106abf4bdddf3b0c3bbc516b244de457ac93a050
a1-soto <a1.angiesoto@gmail.com>
2026-07-04 23:40:26 +0200

    Add semantic structure for components & SEO meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+function Footer() {
+    return(
+        <footer>
+        <img scr={require('../logo.svg')} alt="Little Lemon logo" />
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservations">Reservations</a></li>
+            </ul>
+        </nav>
+
+        <div>
+            <p>Chicago,IL</p>
+            <p>Phone: (123) 456-7890</p>
+            <p>Email: info@littlelemon.com</p>
+        </div>
+
+        <div>
+            <a href="https://facebook.com">Facebook</a>
+            <a href="https://instagram.com">Instagram</a>
+        </div>
+        </footer>
+    );
+}
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+    return(
+        <mai>
+            <h1>Little Lemon</h1>
+        </mai>
+    )
+}
+
+export default Main;
