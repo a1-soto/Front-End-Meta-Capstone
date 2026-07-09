cc8b3b8787dc87213eb8953c27945c7ce5e6c49f
a1-soto <a1.angiesoto@gmail.com>
2026-07-09 23:55:51 +0200

    Header & Hero section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,28 +2,28 @@ import logo from '../assets/images/logo.jpg'
 
 
 function Footer() {
-    return(
+    return (
         <footer>
-        <img src={logo} alt="Little Lemon logo" />
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/menu">Menu</a></li>
-                <li><a href="/reservations">Reservations</a></li>
-            </ul>
-        </nav>
+            <img src={logo} alt="Little Lemon logo" />
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/reservations">Reservations</a></li>
+                </ul>
+            </nav>
 
-        <div>
-            <p>Chicago,IL</p>
-            <p>Phone: (123) 456-7890</p>
-            <p>Email: info@littlelemon.com</p>
-        </div>
+            <div>
+                <p>Chicago,IL</p>
+                <p>Phone: (123) 456-7890</p>
+                <p>Email: info@littlelemon.com</p>
+            </div>
 
-        <div>
-            <a href="https://facebook.com">Facebook</a>
-            <a href="https://instagram.com">Instagram</a>
-        </div>
+            <div>
+                <a href="https://facebook.com">Facebook</a>
+                <a href="https://instagram.com">Instagram</a>
+            </div>
         </footer>
     );
 }
