aef013f10356ab851a0a760382edde60d2a84056
a1-soto <a1.angiesoto@gmail.com>
2026-07-12 00:10:54 +0200

    Add animation to Header, CallToAction & Special

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,25 +1,44 @@
 import "./Card.css";
 
 import { MdDeliveryDining } from "react-icons/md";
+import { forwardRef } from "react";
 
-export default function Card({ image, title, price, description }) {
+
+const Card = forwardRef(function Card(
+  { image, title, price, description },
+  ref
+) {
+
   return (
-    <article className="card">
+    <article
+      className="card"
+      ref={ref}
+    >
+
       <img
         src={image}
         alt={title}
       />
+
       <div className="card-content">
+
         <div className="card-header">
           <h3>{title}</h3>
           <span>{price}</span>
         </div>
+
         <p>{description}</p>
+
         <button className="order-button">
           <span>Order a delivery</span>
           <MdDeliveryDining className="delivery-icon" />
         </button>
+
       </div>
+
     </article>
   );
-}
+});
+
+
+export default Card;
