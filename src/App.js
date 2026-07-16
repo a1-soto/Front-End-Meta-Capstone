bcd54a7c30cddaa6af2f6c42add2ee05004ad3a6
Angelica Soto <a1,angiesoto@gmail.com>
2026-07-16 10:26:26 +0200

    Correction of navbar animation, hover states, logo links home

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import Nav from "./components/Nav/Nav";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
-import { useRef, useLayoutEffect } from "react";
-import gsap from "gsap";
+import { useRef } from "react";
+import { useGsapMatchMedia } from "./hooks/useGsapMatchMedia";
 
 function App() {
   const navbarRef = useRef(null);
-  useLayoutEffect(() => {
-    gsap.fromTo(
-      navbarRef.current,
-      {
-        opacity: 0,
-        y: 30
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 1.8,
-        ease: "power2.out",
-      }
-    );
-  }, []);
+
+  useGsapMatchMedia(
+    navbarRef,
+    (gsap) => {
+      gsap.fromTo(
+        navbarRef.current,
+        { opacity: 0, y: 30 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.7,
+          ease: "power2.out",
+        }
+      );
+    },
+    []
+  );
 
   return (
     <>
-      <div className="navbar">
-        <div className="container" ref={navbarRef}>
+       <div className="navbar">
+        <div className="container" ref={navbarRef} data-animate="true">
           <Header />
           <Nav />
         </div>
