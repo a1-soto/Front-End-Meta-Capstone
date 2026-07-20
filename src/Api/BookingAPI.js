db91b5d2d2424e50109a876846b42b2655afb388
a1-soto <a1.angiesoto@gmail.com>
2026-07-20 18:02:01 +0200

    Initial setting for Apifetch

---
+++ src/Api/BookingAPI.js
@@ -0,0 +1,59 @@
+export const seededRandom = function (seed) {
+
+    const m = 2 ** 35 - 31;
+    const a = 185852;
+    let s = seed % m;
+
+    return function () {
+        return (s = s * a % m) / m;
+    };
+
+};
+
+export const fetchAPI = function (date) {
+    let result = [];
+    let random = seededRandom(date.getDate());
+
+    for (let hour = 12; hour < 15; hour++) {
+        if (random() < 0.5) {
+            result.push(`${hour}:00`);
+        }
+
+        if (random() < 0.5) {
+            result.push(`${hour}:30`);
+        }
+
+    }
+
+
+    if (random() < 0.5) {
+        result.push("15:00");
+    }
+
+    for (let hour = 17; hour < 22; hour++) {
+
+        if (random() < 0.5) {
+            result.push(`${hour}:00`);
+        }
+
+        if (random() < 0.5) {
+            result.push(`${hour}:30`);
+        }
+
+    }
+
+
+    if (random() < 0.5) {
+        result.push("22:00");
+    }
+
+    return result;
+
+};
+
+
+
+export const submitAPI = function (formData) {
+    console.log(formData);
+    return true;
+};
