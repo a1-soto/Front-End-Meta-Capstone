a8bce639b346c00726e808ac148f6a70498440b0
Angelica Soto <a1,angiesoto@gmail.com>
2026-07-20 15:46:52 +0200

    Lift available times state to Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,14 +3,42 @@ import Homepage from "../Homepage/Homepage";
 import BookingPage from "../BookingPage/BookingPage";
 
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 export default function Main() {
+
+  const [availableTimes] = useState([
+  // Lunch
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+
+  // Dinner
+  "17:00",
+  "17:30",
+  "18:00",
+  "18:30",
+  "19:00",
+  "19:30",
+  "20:00",
+  "20:30",
+  "21:00",
+  "21:30",
+  "22:00",
+]);
+
+
   return (
     <main>
       <Routes>
 
         <Route path="/" element={<Homepage />} />
-        <Route path="/reservations" element={<BookingPage />} />
+        <Route path="/reservations"
+         element={<BookingPage availableTimes={availableTimes} />} />
 
       </Routes>
     </main>
