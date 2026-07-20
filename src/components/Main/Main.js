06484ddf0978c7e02b71ea0de9a006f1531d6e84
Angelica Soto <a1,angiesoto@gmail.com>
2026-07-20 23:47:06 +0200

    refactor: simplify reducer to match course's if-based pattern instead of switch

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,43 +2,31 @@ import Homepage from "../Homepage/Homepage";
 import BookingPage from "../BookingPage/BookingPage";
 
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useReducer } from "react";
+import { fetchAPI } from "../../Api/BookingAPI";
+
+function initializeTimes() {
+  return fetchAPI(new Date());
+}
+
+function updateTimes(state, action) {
+  if (action.type === "date_changed") {
+    return fetchAPI(new Date(action.date));
+  }
+  return state;
+}
 
 export default function Main() {
-
-  const [availableTimes] = useState([
-  // Lunch
-  "12:00",
-  "12:30",
-  "13:00",
-  "13:30",
-  "14:00",
-  "14:30",
-  "15:00",
-
-  // Dinner
-  "17:00",
-  "17:30",
-  "18:00",
-  "18:30",
-  "19:00",
-  "19:30",
-  "20:00",
-  "20:30",
-  "21:00",
-  "21:30",
-  "22:00",
-]);
-
+  const [availableTimes, dispatch] = useReducer(updateTimes, [], initializeTimes);
 
   return (
     <main>
       <Routes>
-
         <Route path="/" element={<Homepage />} />
-        <Route path="/reservations"
-         element={<BookingPage availableTimes={availableTimes} />} />
-
+        <Route
+          path="/reservations"
+          element={<BookingPage availableTimes={availableTimes} dispatch={dispatch} />}
+        />
       </Routes>
     </main>
   );
