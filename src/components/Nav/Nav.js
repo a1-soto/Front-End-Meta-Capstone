0c09ecdd06ccc04638785ba29f69d693ef907c2c
a1-soto <a1.angiesoto@gmail.com>
2026-07-21 12:44:28 +0200

    Point About and Menu nav links to homepage anchors, disable out-of-scope links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,20 +9,27 @@ function Nav() {
         <nav aria-label="Primary navigation">
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
+                <li><Link to="/#about">About</Link></li>
+                <li><Link to="/#menu">Menu</Link></li>
                 <li><Link to="/reservations">Reservations</Link></li>
-                <li><Link to="/order-online">Order online</Link></li>
                 <li>
-                    <Link to="/cart" className="nav-cart" aria-label={`Shopping cart, ${cartCount} items`}>
+                    <span className="nav-link-disabled" aria-disabled="true" title="Coming soon">
+                        Order online
+                    </span>
+                </li>
+                <li>
+                    <span className="nav-cart nav-link-disabled" aria-disabled="true" title="Coming soon">
                         <MdShoppingBasket aria-hidden="true" className="nav-basket-icon" />
                         {cartCount > 0 && (
                             <span className="cart-badge" aria-hidden="true">{cartCount}</span>
                         )}
-                    </Link>
+                    </span>
                 </li>
-
-                <li><Link to="/login" className="nav-login">Login</Link></li>
+                <li>
+                    <span className="nav-login nav-link-disabled" aria-disabled="true" title="Coming soon">
+                        Login
+                    </span>
+                </li>
             </ul>
         </nav>
     );
