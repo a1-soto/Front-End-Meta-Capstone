c55ca4a7d53a45242207ed99bddff3a260eea1d2
a1-soto <a1.angiesoto@gmail.com>
2026-07-23 23:04:44 +0200

    Split logo into icon + wordmark, hide wordmark on mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,28 @@
-import "./Header.css"
-import logo from '../../assets/images/logo.jpg'
+import "./Header.css";
+import logoIcon from '../../assets/images/logo-icon.png';
+import logoText from '../../assets/images/logo-text.png';
 import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <header>
-            <Link to="/" aria-label="Little Lemon home">
-                <img src={logo} alt="Little Lemon logo" />
+            <Link to="/" aria-label="Little Lemon home" className="logo-link">
+                <div className="logo-group">
+                    <img
+                        src={logoIcon}
+                        alt=""
+                        className="logo-icon"
+                    />
+
+                    <img
+                        src={logoText}
+                        alt=""
+                        className="logo-text"
+                    />
+                </div>
             </Link>
         </header>
     );
 }
+
 export default Header;
